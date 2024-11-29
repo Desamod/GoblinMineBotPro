1e46d2c267e18bb38b85d8c8d6255957287eb932
Andrey Koptug <16992157+Desamod@users.noreply.github.com>
2024-11-29 23:45:20 +0300

    updated app hash

--- bot/utils/api_checker.py
+++ bot/utils/api_checker.py
@@ -12,7 +12,7 @@ endpoints = ['/graphql']
 
 ws_api_url = 'ws.goblinmine.game'
 ws_key = 'h2co7fdfjnsiwzdrapmq'
-app_hash = '0fad98a8-d6ff-409b-b80d-5e672a4ad2c5'
+app_hash = '4aaa79c6-3498-4441-ae94-f3fb45781588'
 
 
 def find_js_files(base_url):
